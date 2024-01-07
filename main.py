16e1995e6132dca07f13da974a7a6e0d0a611a68
OmerBilgin21 <omrfrkblgn21@gmail.com>
2024-01-07 02:51:16 +0100

    feat: setup multi file app

---
+++ main.py
@@ -0,0 +1,20 @@
+# api
+import uvicorn
+
+# local
+from app import app
+from src.config import get_envs
+
+if __name__ == "__main__":
+    env = get_envs()["env"]
+    if env == "DEV":
+        print("hot reloading")
+        uvicorn.run(
+            "main:app",
+            host="0.0.0.0",
+            port=8080,
+            reload=True,
+            log_level="info",
+        )
+    else:
+        uvicorn.run(app, host="0.0.0.0", port=8080, log_level="info")
